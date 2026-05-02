97f1325bc43dbbd358d6901c775ed31d0272adf7
luceat-lux-vestra <heathkimdev@gmail.com>
2026-05-02 16:40:16 +0900

    Split editor bootstrap from session chunk

--- webview/src/app/bootstrap.ts
+++ webview/src/app/bootstrap.ts
@@ -3,9 +3,8 @@ import "@milkdown/crepe/theme/frame.css";
 import "../style.css";
 import "../styles/mermaid.css";
 
-import {MarkFlowEditorSession} from "./editor-session";
-
 export const bootstrapMarkFlowEditor = async () => {
+    const {MarkFlowEditorSession} = await import("./editor-session");
     const session = new MarkFlowEditorSession();
     await session.initEditor();
 };
