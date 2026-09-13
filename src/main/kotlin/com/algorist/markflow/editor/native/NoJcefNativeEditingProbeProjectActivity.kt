9e5ea2b23a64868dc9d410dcbd19034647a7b9bd
luceat-lux-vestra <heathkimdev@gmail.com>
2026-09-13 18:23:26 +0900

    test(editor): route gesture hardening probe

--- src/main/kotlin/com/algorist/markflow/editor/native/NoJcefNativeEditingProbeProjectActivity.kt
+++ src/main/kotlin/com/algorist/markflow/editor/native/NoJcefNativeEditingProbeProjectActivity.kt
@@ -9,6 +9,7 @@ class NoJcefNativeEditingProbeProjectActivity : ProjectActivity {
         val imageImportRequested = System.getProperty(NativeImageImportProbe.OUTPUT_PROPERTY)
             ?.isNotBlank() == true
         if (imageImportRequested) {
+            NativeImageImportGestureProbe.runIfRequested(project)
             NativeImageImportTransactionProbe.runIfRequested(project)
             NativeImageImportProbe.runIfRequested(project)
         } else {
