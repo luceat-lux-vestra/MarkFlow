dcdbf5688b55958113d515c7861f660075ec5c9a
luceat-lux-vestra <heathkimdev@gmail.com>
2026-09-15 07:22:51 +0900

    test(e2e): add real native editor interactions

--- src/integrationTest/kotlin/com/algorist/markflow/e2e/MarkFlowIdeDriver.kt
+++ src/integrationTest/kotlin/com/algorist/markflow/e2e/MarkFlowIdeDriver.kt
@@ -3,34 +3,57 @@ package com.algorist.markflow.e2e
 import com.intellij.driver.client.Driver
 import com.intellij.driver.client.Remote
 import com.intellij.driver.client.service
-import com.intellij.driver.sdk.Document
-import com.intellij.driver.sdk.VirtualFile
-import com.intellij.driver.sdk.findOpenFile
+import com.intellij.driver.sdk.invokeAction
 import com.intellij.driver.sdk.openFile
+import com.intellij.driver.sdk.ui.components.common.JEditorUiComponent
+import com.intellij.driver.sdk.ui.components.common.codeEditorForFile
+import com.intellij.driver.sdk.ui.components.common.ideFrame
+import com.intellij.driver.sdk.waitFor
+import kotlin.time.Duration.Companion.seconds
 
 /**
  * MarkFlow-owned boundary around the experimental JetBrains Driver API.
  * Keep this adapter limited to behavior exercised by the current acceptance slice.
  */
 class MarkFlowIdeDriver(private val driver: Driver) {
-    fun openMarkdown(fileName: String): Document {
-        driver.openFile(fileName, waitForCodeAnalysis = false, isTextEditor = false)
-        val file = checkNotNull(driver.findOpenFile(fileName, isTextEditor = false)) {
-            "opened file not found after production FileEditorManager open: $fileName"
-        }
-        return checkNotNull(driver.service<FileDocumentManagerRemote>().getDocument(file)) {
-            "authoritative Document unavailable for opened file: $fileName"
+    fun openMarkdown(fileName: String): JEditorUiComponent {
+        driver.openFile(fileName, waitForCodeAnalysis = false, isTextEditor = true)
+        return driver.ideFrame().codeEditorForFile(fileName).also { editor ->
+            check(editor.isEditable()) { "opened Markdown editor is not editable: $fileName" }
         }
     }
 
-    fun source(document: Document): String = document.getText()
+    fun source(editor: JEditorUiComponent): String = editor.document.getText()
 
-    fun isDirty(document: Document): Boolean =
-        driver.service<FileDocumentManagerRemote>().isDocumentUnsaved(document)
+    fun isDirty(editor: JEditorUiComponent): Boolean =
+        driver.service<FileDocumentManagerRemote>().isDocumentUnsaved(editor.document)
+
+    fun appendAtEnd(editor: JEditorUiComponent, text: String) {
+        editor.setFocus()
+        editor.moveCaretToOffset(editor.text.length)
+        editor.keyboard {
+            typeText(text, delayBetweenCharsInMs = 20)
+        }
+    }
+
+    fun save(editor: JEditorUiComponent) {
+        driver.invokeAction("SaveAll", component = editor.component)
+        waitFor(
+            message = "Markdown document is saved",
+            timeout = 10.seconds,
+            getter = { isDirty(editor) },
+            checker = { dirty -> !dirty },
+        )
+    }
+
+    fun close(editor: JEditorUiComponent) {
+        editor.setFocus()
+        driver.invokeAction("CloseContent", component = editor.component)
+        editor.waitNotFound(10.seconds)
+    }
 }
 
 @Remote("com.intellij.openapi.fileEditor.FileDocumentManager")
 private interface FileDocumentManagerRemote {
-    fun getDocument(file: VirtualFile): Document?
-    fun isDocumentUnsaved(document: Document): Boolean
+    fun isDocumentUnsaved(document: com.intellij.driver.sdk.Document): Boolean
 }
