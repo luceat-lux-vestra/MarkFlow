a62eabc3f0179cad6005d707be289feec51a2fb9
luceat-lux-vestra <heathkimdev@gmail.com>
2026-09-15 18:32:41 +0900

    test(e2e): add real editor formatting interactions

--- src/integrationTest/kotlin/com/algorist/markflow/e2e/MarkFlowIdeDriver.kt
+++ src/integrationTest/kotlin/com/algorist/markflow/e2e/MarkFlowIdeDriver.kt
@@ -15,6 +15,7 @@ import com.intellij.driver.sdk.ui.components.common.editor
 import com.intellij.driver.sdk.ui.components.common.ideFrame
 import com.intellij.driver.sdk.ui.remote.Component
 import com.intellij.driver.sdk.waitFor
+import java.awt.event.KeyEvent
 import kotlin.time.Duration.Companion.seconds
 
 /**
@@ -69,6 +70,35 @@ class MarkFlowIdeDriver(private val driver: Driver) {
     fun clickText(editor: JEditorUiComponent, text: String) {
         editor.setFocus()
         editor.clickOn(text)
+    }
+
+    fun selectRangeWithKeyboard(editor: JEditorUiComponent, startOffset: Int, length: Int) {
+        require(length > 0) { "keyboard selection length must be positive" }
+        editor.setFocus()
+        editor.moveCaretToOffset(startOffset)
+        editor.keyboard {
+            pressing(KeyEvent.VK_SHIFT) {
+                repeat(length) { right() }
+            }
+        }
+    }
+
+    fun invokeMarkdownBold(editor: JEditorUiComponent) {
+        editor.setFocus()
+        driver.invokeAction(
+            "org.intellij.plugins.markdown.ui.actions.styling.ToggleBoldAction",
+            component = editor.component,
+        )
+    }
+
+    fun undo(editor: JEditorUiComponent) {
+        editor.setFocus()
+        driver.invokeAction("\$Undo", component = editor.component)
+    }
+
+    fun redo(editor: JEditorUiComponent) {
+        editor.setFocus()
+        driver.invokeAction("\$Redo", component = editor.component)
     }
 
     fun attachNativeProjection(editor: JEditorUiComponent) {
